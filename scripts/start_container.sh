63eebc56fed2a555233ba967e1ec5e428d75cd03
edgeconnect-codecommit-user01-at-381492305313 <edgeconnect-codecommit-user01-at-381492305313>
2024-06-05 18:12:45 +0100

    slight modification - fourth commit

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# Pull the Docker image from my Docker Hub
 docker pull techkedgec0nnect/techkedgeconnect-simple-python-flask-app
 
 # Run the Docker image as a container
